3fbbe8595437bf419aa5fc8b3dcc43187f5274e0
Ewerton Luna <ewerton.al@gmail.com>
2019-11-27 10:45:26 -0300

    Atualiza classe

--- model/Gerente.java
+++ model/Gerente.java
@@ -1,8 +1,6 @@
 package model;
 
 import exception.GerenteException;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Gerente extends Funcionario {
     private Solicitacao solicitacao;
