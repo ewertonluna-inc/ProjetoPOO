f6713cd4de44dec25b22cf7bacf7078fc4dca485
Ewerton Luna <ewerton.al@gmail.com>
2019-11-27 10:42:17 -0300

    Conclui classe Solicitacao

--- model/Solicitacao.java
+++ model/Solicitacao.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Solicitacao {
+    private String mensagem;
+    private boolean isResolvida;
 
+    public Solicitacao(String mensagem) {
+        this.mensagem = mensagem;
+        isResolvida = false;
+    }
+
+    public void resolverSolicitacao() {
+        this.isResolvida = true;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public boolean isResolvida() {
+        return isResolvida;
+    }
+
+    @Override
+    public String toString() {
+        return "Solicitacao [isResolvida=" + isResolvida + ", mensagem=" + mensagem + "]";
+    }
+
+    
 }
+
