5db781e8e32f05732546244c5af4a9d058359349
Ewerton Luna <ewerton.al@gmail.com>
2019-11-27 10:40:16 -0300

    Cria método inserirSolicitacao

--- model/Gerente.java
+++ model/Gerente.java
@@ -1,18 +1,44 @@
 package model;
 
+import exception.GerenteException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Gerente extends Funcionario {
-    // Solicitacao solicitacao;
+    Solicitacao solicitacao;
 
 
-    Gerente(String nome, String cpf, String empresa, byte tempoDeExperiencia) {
+    public Gerente(String nome, String cpf, String empresa, byte tempoDeExperiencia) {
         super(nome, cpf, empresa, tempoDeExperiencia);
+        this.solicitacao = null;
     }
 
-    public void resolverSolicitaçao() {
-        // todo: terminar
+    public void inserirSolicitacao(Solicitacao solicitacao) throws GerenteException{
+        if (solicitacao == null) {
+            throw new GerenteException("null não é um argumento válido");
+        }
+
+        if (this.solicitacao != null) {
+            if (this.solicitacao.isResolvida()) {
+                this.solicitacao = solicitacao;
+            } else {
+                throw new GerenteException("Gerente possui solicitacao em aberto.");
+            }
+        } else {
+            this.solicitacao = solicitacao;
+        }
+    }
+
+    public void resolverSolicitacao() throws GerenteException{
+        if (solicitacao == null) {
+            throw new GerenteException("Gerente não possui solicitações.");
+        } else {
+            solicitacao.resolverSolicitacao();
+        }
+    }
+
+    public Solicitacao getSolicitacao() {
+        return this.solicitacao;
     }
 
     @Override
@@ -20,5 +46,15 @@ public class Gerente extends Funcionario {
         
         return 0;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Gerente [solicitacao=" + solicitacao + "]";
+    }
+
+     
+
+    
     
 }
+
