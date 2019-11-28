71db607f3a6cf6c3550074e02e56f0c239950589
Ewerton Luna <ewerton.al@gmail.com>
2019-11-28 16:52:39 -0300

    Cria pacote

---
+++ repositorio/IRepositorioCliente.java
@@ -0,0 +1,11 @@
+package repositorio;
+
+import model.Cliente;
+import exception.ClienteException;
+
+public interface IRepositorioCliente {
+    void inserirCliente(Cliente cliente) throws ClienteException;
+    Cliente procurarCliente(String cpf);
+    void removerCliente(String cpf) throws ClienteException;
+    void atualizarCliente(Cliente cliente) throws ClienteException;
+}
