2b4ecde65314b12c427fee665524b847a41f5935
Ewerton Luna <ewerton.al@gmail.com>
2019-11-30 23:34:49 -0300

    Inicia classe

--- ui/Principal.java
+++ ui/Principal.java
@@ -1,19 +1,65 @@
 package ui;
 
-import model.Insumo;
-import model.Lanche;
+import java.util.Scanner;
+
+import controlador.*;
+import model.*;
+import repositorio.*;
+import exception.*;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
-        Lanche hamburguer = new Lanche("Hamburguer");
-        Insumo insumo1 = new Insumo("alface", 10, 2, 2);
-        Insumo insumo2 = new Insumo("alface", 10, 2, 2);
-        Insumo insumo3 = new Insumo("tomate", 10, 5, 3);
-        hamburguer.adicionarInsumo(insumo1);
-        hamburguer.adicionarInsumo(insumo2);
-        hamburguer.adicionarInsumo(insumo3);
+        Scanner scanner = new Scanner(System.in);
+        boolean sair = false;
+        String opcao;
 
-        System.out.println(hamburguer.getPreco());
-        
+        while (!sair) {
+            System.out.println("*** Menu Principal ***");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Cadastro de Balconista");
+            System.out.println("2 - Cadastro de Cozinheiro");
+            System.out.println("3 - Cadastro de Gerente");
+            System.out.println("4 - Cadastro de Cliente");
+            System.out.println("5 - Cadastro de Pedido");
+            System.out.println("6 - Cadastro de Insumos");
+            System.out.println("7 - Menu Balconista");
+            System.out.println("8 - Menu Cozinheiro");
+            System.out.println("9 - Menu Gerente");
+            System.out.println();
+            System.out.print("Opcão >> ");
+
+            opcao = scanner.nextLine();
+            
+            if (opcao.equals("0")) {
+                sair = true;
+            } else if (opcao.equals("1")) {
+                //UICadastroCliente uiCadastroCliente = new UICadastrCliente();
+                // uiCadastroCliente.showMenu();
+            } else if (opcao.equals("2")) {
+                //UICadastroCozinheiro uiCadastroCozinheiro = new UICadastroCozinheiro();
+                // uiCadastroCozinheiro.showMenu();
+            } else if (opcao.equals("3")) {
+                // UICadastroGerente uiCadastroGerente = new UICadastroGerente();
+                // uiCadastroGerente.showMenu();
+            } else if (opcao.equals("4")) {
+                // UICadastroCliente uiCadastroCliente = new UICadastroCliente();
+                // uiCadastroCliente.showMenu();
+            } else if (opcao.equals("5")) {
+                // UICadastroPedido uiCadastroPedido = new UICadastroPedido();
+                // uiCadastroPedido.showMenu();
+            } else if (opcao.equals("6")) {
+                // UICadastroInsumo uiCadastroInsumo = new UICadastroInsumo();
+                // uiCadastroInsumo.showMenu();
+            } else if (opcao.equals("7")) {
+                // UIBalconista uiBalconista = new UIBalconista();
+                // uiBalconista.showMenu();
+            } else if (opcao.equals("8")) {
+                // UICozinheiro uiCozinheiro = new UICozinheiro();
+                // uiCozinheiro.showMenu();
+            } else if (opcao.equals("9")) {
+                // UIGerente uiGerente = new UIGerente();
+                // uiGerente.showMenu();
+            }
+        }
     }
 }
