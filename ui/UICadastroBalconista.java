821655118a313fb0d71adb03eb82b4c7c40000c6
Ewerton Luna <ewerton.al@gmail.com>
2019-12-01 10:28:55 -0300

    Conclui método inserirBalconista()

--- ui/UICadastroBalconista.java
+++ ui/UICadastroBalconista.java
@@ -1,6 +1,9 @@
 package ui;
 
 import java.util.Scanner;
+import controlador.Fachada;
+import exception.BalconistaException;
+import model.Balconista;
 
 public class UICadastroBalconista implements IMenu {
     private static Scanner scanner = new Scanner(System.in);
@@ -17,14 +20,15 @@ public class UICadastroBalconista implements IMenu {
             System.out.println("2 - Remover Balconista");
             System.out.println("3 - Procurar Balconista");
             System.out.println("4 - Atualizar Balconista");
-            System.out.println("Opção >> ");
+            System.out.print("Opção >> ");
 
             opcao = scanner.nextLine();
+            System.out.println();
 
             if (opcao.equals("0")) {
                 sair = true;
             } else if (opcao.equals("1")) {
-                // inserirBalconista();
+                inserirBalconista();
             } else if (opcao.equals("2")) {
                 // removerBalconista();
             } else if (opcao.equals("3")) {
@@ -36,16 +40,47 @@ public class UICadastroBalconista implements IMenu {
 
     }
 
-    private void atualizarBalconista() {
-    }
-
     private void inserirBalconista() {
+        String nome, cpf, empresa;
+        String aux;
+        byte tempoDeExperiencia;
+        
+        System.out.print("Nome: ");
+        nome = scanner.nextLine();
+        System.out.print("CPF: ");
+        cpf = scanner.nextLine();
+        System.out.print("Empresa: ");
+        empresa = scanner.nextLine();
+        System.out.print("Tempo de experiência: ");
+        aux = scanner.nextLine();
+        
+        try {
+            tempoDeExperiencia = Byte.parseByte(aux);   
+        } catch (Exception e) {
+            System.out.println("Erro de input: " + e.getMessage());
+            System.out.println();
+            return;
+        }
+
+        Balconista balconista = new Balconista(nome, cpf, empresa, tempoDeExperiencia); 
+        
+        try {
+            Fachada.getInstancia().inserirBalconista(balconista);
+            System.out.println("Balconista inserido com sucesso!");
+            System.out.println();
+        } catch (BalconistaException e) {
+            System.out.println("Erro ao inserir balconista: " + e.getMessage());
+            System.out.println();
+        }
     }
 
     private void removerBalconista() {
     }
 
     private void procurarBalconista() {
+    }
+
+    private void atualizarBalconista() {
     }
     
 }
