3753229c7e26a21186fa34024fac6459f636c35d
Ewerton Luna <ewerton.al@gmail.com>
2019-12-02 16:36:21 -0300

    Atualiza classe

--- util/Estoque.java
+++ util/Estoque.java
@@ -7,4 +7,40 @@ public final class Estoque {
     public static final Insumo ALFACE = new Insumo("alface", 1.5, 0.5, 100);
     public static final Insumo CARNE = new Insumo("carne", 98, 6, 100);
     public static final Insumo PAO = new Insumo("pao", 142, 1, 100);
+
+    public static Insumo createTomate() {
+        String nome = TOMATE.getNome();
+        double caloriasIndividual = TOMATE.getCaloriasIndividual();
+        double precoIndividual = TOMATE.getPrecoIndividual();
+        int quantidade = TOMATE.getQuantidade();
+
+        return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
+    }
+
+    public static Insumo createAlface() {
+        String nome = ALFACE.getNome();
+        double caloriasIndividual = ALFACE.getCaloriasIndividual();
+        double precoIndividual = ALFACE.getPrecoIndividual();
+        int quantidade = ALFACE.getQuantidade();
+
+        return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
+    }
+
+    public static Insumo createCarne() {
+        String nome = CARNE.getNome();
+        double caloriasIndividual = CARNE.getCaloriasIndividual();
+        double precoIndividual = CARNE.getPrecoIndividual();
+        int quantidade = CARNE.getQuantidade();
+
+        return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
+    }
+
+    public static Insumo createPao() {
+        String nome = PAO.getNome();
+        double caloriasIndividual = PAO.getCaloriasIndividual();
+        double precoIndividual = PAO.getPrecoIndividual();
+        int quantidade = PAO.getQuantidade();
+
+        return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
+    }
 }
