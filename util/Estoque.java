838443a50eeb8dc4b6944b6a619a30778695ba14
Ewerton Luna <ewerton.al@gmail.com>
2019-12-02 15:56:58 -0300

    Cria classe final

---
+++ util/Estoque.java
@@ -0,0 +1,10 @@
+package util;
+
+import model.Insumo;
+
+public final class Estoque {
+    public static final Insumo TOMATE = new Insumo("tomate", 4, 1.5, 100);
+    public static final Insumo ALFACE = new Insumo("alface", 1.5, 0.5, 100);
+    public static final Insumo CARNE = new Insumo("carne", 98, 6, 100);
+    public static final Insumo PAO = new Insumo("pao", 142, 1, 100);
+}
