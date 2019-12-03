4679356213f49d894aa67e2dbb04ab649897110b
Ewerton Luna <ewerton.al@gmail.com>
2019-12-02 22:03:39 -0300

    Atualiza classe

--- util/Estoque.java
+++ util/Estoque.java
@@ -7,6 +7,8 @@ public final class Estoque {
     public static final Insumo ALFACE = new Insumo("alface", 1.5, 0.5, 100);
     public static final Insumo CARNE = new Insumo("carne", 98, 6, 100);
     public static final Insumo PAO = new Insumo("pao", 142, 1, 100);
+    public static final Insumo QUEIJO = new Insumo("queijo", 27, .2, 100);
+    public static final Insumo BACON = new Insumo("bacon", 40, 1.1, 100);
 
     public static Insumo createTomate() {
         String nome = TOMATE.getNome();
@@ -40,6 +42,24 @@ public final class Estoque {
         double caloriasIndividual = PAO.getCaloriasIndividual();
         double precoIndividual = PAO.getPrecoIndividual();
         int quantidade = PAO.getQuantidade();
+
+        return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
+    }
+
+    public static Insumo createQueijo() {
+        String nome = QUEIJO.getNome();
+        double caloriasIndividual = QUEIJO.getCaloriasIndividual();
+        double precoIndividual = QUEIJO.getPrecoIndividual();
+        int quantidade = QUEIJO.getQuantidade();
+
+        return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
+    }
+
+    public static Insumo createBacon() {
+        String nome = BACON.getNome();
+        double caloriasIndividual = BACON.getCaloriasIndividual();
+        double precoIndividual = BACON.getPrecoIndividual();
+        int quantidade = BACON.getQuantidade();
 
         return new Insumo(nome, caloriasIndividual, precoIndividual, quantidade);
     }
