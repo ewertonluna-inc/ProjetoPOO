639dfb9cd8a76202d6ce8389a36575d5640a5612
Ewerton Luna <ewerton.al@gmail.com>
2019-12-04 15:20:58 -0300

    Muda classe de final para abstract

--- util/Estoque.java
+++ util/Estoque.java
@@ -2,7 +2,7 @@ package util;
 
 import model.Insumo;
 
-public final class Estoque {
+public abstract class Estoque {
     public static final Insumo TOMATE = new Insumo("tomate", 4, 1.5, 100);
     public static final Insumo ALFACE = new Insumo("alface", 1.5, 0.5, 100);
     public static final Insumo CARNE = new Insumo("carne", 98, 6, 100);
